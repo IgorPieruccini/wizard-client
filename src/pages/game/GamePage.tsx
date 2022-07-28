7ded1c0ea7a6eb38b2e1aa2272dc95e941ab9933
igor pieruccini <igor.pieruccini@sosafe.de>
2022-07-28 22:28:02 +0200

    💄 update game page style

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -1,18 +1,27 @@
+import styled from "styled-components"
 import { PlayerHand } from "./components/PlayerHand"
 import { PlayersLobby } from "./components/PlayersLobby"
 import { UntieCard } from "./components/UntieCard"
+
+
+const GamePageContainer = styled.div`
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+`
+
 
 export const GamePage = ()=> {
 
 
     return (
-        <div>
-            <p>Game Page</p>
+        <GamePageContainer>
             <PlayersLobby /> 
             <br/>
             <UntieCard />
             <br/>
             <PlayerHand />
-        </div>
+        </GamePageContainer>
     )     
 }
